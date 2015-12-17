66c3ff86e575ff2d10689b720c7cf9292bbd7d7c
thierry <thierry@apprensemble.link>
2015-12-17 16:30:17 +0000

    version preparation exercice 1

---
+++ evenement.h
@@ -0,0 +1,19 @@
+﻿#if !defined(_EVENEMENT_H) 
+#define _EVENEMENT_H 
+#include <iostream> 
+#include <string> 
+#include "timing.h"
+namespace TIME{
+  class Evt1j {
+    private :
+      Date date;
+      std::string sujet;
+    public : Evt1j( const Date& d, const std::string& s):date(d),sujet(s){}
+	     const std::string& getDescription() const { return sujet; }
+	     const Date& getDate() const { return date; }
+	     void afficher(std::ostream& f= std::cout) const {
+	       f<<" ***** Evt ******** "<<"\n"<<"Date="<<date<<" sujet="<<sujet<<"\n";
+	     }
+  };
+} 
+#endif
